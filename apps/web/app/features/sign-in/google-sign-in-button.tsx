392cc3933ac88743a5968bf76d030bec19693691
wnmay <wanathachat@gmail.com>
2025-07-01 20:32:07 +0700

    feat: add loading state in log in button

--- apps/web/app/features/sign-in/google-sign-in-button.tsx
+++ apps/web/app/features/sign-in/google-sign-in-button.tsx
@@ -1,17 +1,41 @@
+import { Loader2Icon } from "lucide-react";
+import { useState } from "react";
 import { Button } from "~/components/ui/button";
 
 export function GoogleSignInButton() {
+  const [isLoading, setIsLoading] = useState(false);
   const buttonOnClick = () => {
-    console.log("click");
+    setIsLoading(true);
+
+    //awaiting for oauth token
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 1000);
   };
   return (
-    <Button
-      onClick={buttonOnClick}
-      variant="outline"
-      className="py-7 text-2xl font-normal text-black/50 gap-3"
-    >
-      <img src="/base_resource/google_logo.svg"></img>
-      Sign in With Google
-    </Button>
+    <>
+      {isLoading ? (
+        <Button
+          variant={"secondary"}
+          className="text-2xl font-normal text-black/50 py-7 border-1"
+          disabled
+        >
+          <Loader2Icon
+            className="animate-spin"
+            style={{ width: "2rem", height: "2rem" }}
+          />
+          Loading
+        </Button>
+      ) : (
+        <Button
+          onClick={buttonOnClick}
+          variant="outline"
+          className="py-7 text-2xl font-normal text-black/50 gap-3 hover:text-black/50"
+        >
+          <img src="/base_resource/google_logo.svg" />
+          Sign in With Google
+        </Button>
+      )}
+    </>
   );
 }
