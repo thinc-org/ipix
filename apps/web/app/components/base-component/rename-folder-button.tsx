a835b4f59f4917acdf2653d1c20f422a1561c8c9
wnmay <wanathachat@gmail.com>
2025-07-07 18:18:27 +0700

    feat: rename folder button

---
+++ apps/web/app/components/base-component/rename-folder-button.tsx
@@ -0,0 +1,52 @@
+import { useRef, useState } from "react";
+import { Button } from "../ui/button";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "../ui/dialog";
+
+export function RenameFolderButton({ className }: { className?: string }) {
+  const inputNewFolderName = useRef<HTMLInputElement>(null);
+  const [open, setOpen] = useState(false);
+  const createFolder = () => {
+    const newFolderName = inputNewFolderName.current?.value || "";
+    if (newFolderName) {
+      console.log(`Folder Name: ${newFolderName}`);
+      setOpen(false);
+    }
+  };
+  return (
+    <Dialog open={open} onOpenChange={setOpen}>
+      <DialogTrigger asChild>
+        <Button className={className}>Rename</Button>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Rename</DialogTitle>
+          <input
+            ref={inputNewFolderName}
+            type="text"
+            className="gap-10 p-2 rounded-md border-[0.5px] border-black mt-8"
+          />
+        </DialogHeader>
+        <div className="flex justify-end space-x-2 mt-4 gap-4">
+          <DialogClose asChild>
+            <button className="bg-transparent text-black/50 text-sm">
+              Cancel
+            </button>
+          </DialogClose>
+          <button
+            className="bg-transparent text-blue-500 text-sm"
+            onClick={createFolder}
+          >
+            Save
+          </button>
+        </div>
+      </DialogContent>
+    </Dialog>
+  );
+}
