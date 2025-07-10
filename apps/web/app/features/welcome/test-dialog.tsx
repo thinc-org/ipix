0dca4b4ebb6b9f8cb0a1c042e474880f3183b857
neennera <neennera@gmail.com>
2025-07-10 21:07:42 +0700

    chore : fix base_component

--- apps/web/app/features/welcome/test-dialog.tsx
+++ apps/web/app/features/welcome/test-dialog.tsx
@@ -16,7 +16,7 @@ import {
   CardTitle,
 } from "~/components/ui/card";
 import { Label } from "~/components/ui/label";
-import { Header } from "~/components/base_component/header";
+import { Header } from "~/components/base-component/header";
 import { DialogRename } from "../folder/dialog-rename";
 
 export function TestDialog() {
