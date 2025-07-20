e031a4ad6d20e4d2d518f0e165f6d05dcc8b829c
wnmay <wanathachat@gmail.com>
2025-07-20 21:34:21 +0700

    feat: add profile editing ui

--- apps/web/src/components/base-component/user-account-card.tsx
+++ apps/web/src/components/base-component/user-account-card.tsx
@@ -4,11 +4,11 @@ import {
   Dialog,
   DialogClose,
   DialogContent,
-  DialogHeader,
   DialogTrigger,
 } from "../ui/dialog";
 import { buttonVariants } from "../ui/button";
 import type { VariantProps } from "class-variance-authority";
+import { Pencil } from "lucide-react";
 
 type ButtonVariant = VariantProps<typeof buttonVariants>["variant"];
 
@@ -74,8 +74,8 @@ export function UserAccountCard({
         <Button variant={variant}>Account</Button>
       </DialogTrigger>
       <DialogContent>
-        <DialogHeader>
-          <div className="flex flex-col items-center mt-4">
+        <div className="flex flex-col items-center mt-4">
+          <div className="relative w-20 h-20 mb-4">
             <img
               src={
                 editing
@@ -83,49 +83,58 @@ export function UserAccountCard({
                   : userInfo.imageProfile || "/default-profile.png"
               }
               alt={userInfo.name || "User"}
-              className={`w-20 h-20 rounded-full object-cover mb-4 ${editing ? "cursor-pointer" : ""}`}
+              className={`w-full h-full rounded-full object-cover ${
+                editing ? "cursor-pointer" : ""
+              }`}
               onClick={() =>
                 editing && document.getElementById("image-input")?.click()
               }
             />
-            <input
-              id="image-input"
-              type="file"
-              accept="image/*"
-              onChange={handleImageChange}
-              className="hidden"
-            />
+
+            {editing && (
+              <div className="absolute bottom-0 right-0 bg-white rounded-full p-1 shadow border-2">
+                <Pencil className="w-4 h-4 text-gray-500" />
+              </div>
+            )}
           </div>
-          <div className="mt-4">
-            <div className="flex justify-between mb-4">
-              <div className="text-lg w-1/2">Name</div>
-              <div className="w-1/2 text-right">
-                {editing ? (
-                  <input
-                    type="text"
-                    value={tempName || ""}
-                    onChange={(e) => setTempName(e.target.value)}
-                    className="text-lg text-gray-600 border border-gray-300 p-1 w-full"
-                  />
-                ) : (
-                  <span className="text-lg text-gray-600">{userInfo.name}</span>
-                )}
-              </div>
-            </div>
-            <div className="flex justify-between mb-4">
-              <div className="text-lg w-1/2">Role</div>
-              <div className="text-lg text-gray-600 w-1/2 text-right">
-                {userInfo.role}
-              </div>
-            </div>
-            <div className="flex justify-between mb-4">
-              <div className="text-lg w-1/2">Email</div>
-              <div className="text-lg text-gray-600 w-1/2 text-right">
-                {userInfo.email}
-              </div>
+
+          <input
+            id="image-input"
+            type="file"
+            accept="image/*"
+            onChange={handleImageChange}
+            className="hidden"
+          />
+        </div>
+        <div className="mt-4">
+          <div className="flex justify-between mb-4">
+            <div className="text-lg w-1/2">Name</div>
+            <div className="w-1/2 text-right">
+              {editing ? (
+                <input
+                  type="text"
+                  value={tempName || ""}
+                  onChange={(e) => setTempName(e.target.value)}
+                  className="text-lg text-gray-600 border border-gray-300 p-1 w-full"
+                />
+              ) : (
+                <span className="text-lg text-gray-600">{userInfo.name}</span>
+              )}
             </div>
           </div>
-        </DialogHeader>
+          <div className="flex justify-between mb-4">
+            <div className="text-lg w-1/2">Role</div>
+            <div className="text-lg text-gray-600 w-1/2 text-right">
+              {userInfo.role}
+            </div>
+          </div>
+          <div className="flex justify-between mb-4">
+            <div className="text-lg w-1/2">Email</div>
+            <div className="text-lg text-gray-600 w-1/2 text-right">
+              {userInfo.email}
+            </div>
+          </div>
+        </div>
         <div className="flex justify-end space-x-2 mt-4">
           {editing ? (
             <>
