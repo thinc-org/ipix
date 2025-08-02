a7a9bbad0ef00574c674cced27e4ae71776743e8
FaiiFaiipuipui <pakarnpahulo2003@gmail.com>
2025-08-02 23:04:51 +0700

    feat: Update schema to include storage table alongside authentication table

--- packages/rdb/src/schema.ts
+++ packages/rdb/src/schema.ts
@@ -1,3 +1,5 @@
 import { user, session, account, verification } from "./schemas/auth";
+import { item, space, accessRank } from "./schemas/storage";
 
-export const authTable = { user, session, account, verification, };
+export const storageTable = { item, space, accessRank };
+export const authTable = { user, session, account, verification };
