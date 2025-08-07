de85afb7129615e486ab68cbb231e3dde270fdd8
FaiiFaiipuipui <pakarnpahulo2003@gmail.com>
2025-08-07 19:13:59 +0700

    refactor: Simplify exports for storage and auth tables in schema.ts

--- packages/rdb/src/schema.ts
+++ packages/rdb/src/schema.ts
@@ -1,5 +1,2 @@
-import { user, session, account, verification } from "./schemas/auth";
-import { item, space, accessRank } from "./schemas/storage";
-
-export const storageTable = { item, space, accessRank };
-export const authTable = { user, session, account, verification };
+export * as storageTable from "./schemas/storage";
+export * as authTable    from "./schemas/auth";
