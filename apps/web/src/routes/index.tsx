d64ca2489dc4210ad936738c91ad3615c4544590
FaiiFaiipuipui <pakarnpahulo2003@gmail.com>
2025-08-07 14:16:59 +0700

    feat: Add authentication loader to redirect unauthenticated users to sign-in

--- apps/web/src/routes/index.tsx
+++ apps/web/src/routes/index.tsx
@@ -1,9 +1,18 @@
-import { createFileRoute } from "@tanstack/react-router";
+import { createFileRoute, redirect } from "@tanstack/react-router";
 import { TestDialog } from "@/features/welcome/test-dialog";
 import { TestUi } from "@/features/welcome/test-ui";
 import { HomePage } from "@/features/welcome/home-page";
+import { getSessionServerFn } from "@/lib/better-auth/auth-server-fns";
 
 export const Route = createFileRoute("/")({
+  loader: async () => {
+    const session = await getSessionServerFn()
+    if (!session?.user) {
+      throw redirect({
+        to: '/sign-in'
+      })
+    }
+  },
   component: App,
 });
 
